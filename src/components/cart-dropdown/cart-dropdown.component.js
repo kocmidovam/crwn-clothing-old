0660fb5913b0eca5c7234622ef9baf6cee8852a7
Marta Kocmidova <marta.kocmidova@socialbakers.com>
2019-09-29 16:13:45 +0200

    checkout page in progress

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -4,20 +4,25 @@ import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 import "./cart-dropdown.styles.scss"
+import {createStructuredSelector} from "reselect";
 
-const Cart = ({ cartItems }) => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      {cartItems.map(cartItem => (
+      {cartItems.length ? (
+        cartItems.map(cartItem => (
           <CartItem key={cartItem.id} item={cartItem}/>
-        ))}
+        ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+           )}
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 )
 
-const mapStateToProps = state => ({
-  cartItems: selectCartItems(state)
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems
 })
 
-export default connect(mapStateToProps)(Cart)
+export default connect(mapStateToProps)(CartDropdown)
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,9 +2,12 @@ import React from "react"
 import { Link } from "react-router-dom"
 import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { createStructuredSelector } from "reselect";
 import { connect } from 'react-redux';
 import CartIcon from "../cart-icon/cart-icon.component";
-import CartDropdown from "../cart-dropdown/cart-dropdown.components"
+import CartDropdown from "../cart-dropdown/cart-dropdown.component"
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selector";
 
 import "./header.styles.scss"
 
@@ -32,9 +35,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header)
