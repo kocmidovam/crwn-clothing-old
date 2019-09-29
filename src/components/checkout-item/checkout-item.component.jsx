aa2e043933b13c6d510611306e9034365cb8a63c
Marta Kocmidova <mochomurka6@seznam.cz>
2019-09-29 18:10:49 +0200

    checkout item in progress

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "./checkout-item.styles.scss"
+
+const CheckoutItem = () => (
+  <div className="checkout-item">
+    <div className="image-continer">
+      <img src={} />
+    </div>
+  </div>
+)
