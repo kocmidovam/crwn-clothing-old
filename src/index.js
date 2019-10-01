cf07eff683890cad31b76768e40cf45605e869a5
Marta Kocmidova <marta.kocmidova@socialbakers.com>
2019-10-01 16:55:57 +0200

    added redux-persist to cart store

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import { BrowserRouter } from "react-router-dom"
 import './index.css';
 import App from './App';
 import { Provider } from "react-redux"
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import {store, persistor} from "./redux/store";
 
 ReactDOM.render(
   // provider is component from redux which i need to use to provide state
   // obalí naší aplikaci a předá store
   <Provider store={store} >
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
     document.getElementById('root')
