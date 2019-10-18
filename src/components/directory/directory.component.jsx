184f924428f6e1d52bcd5140237d675d6a0512db
Marta Kocmidova <marta.kocmidova@socialbakers.com>
2019-10-18 11:17:38 +0200

    directory sections separated in reducer

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { connect } from "react-redux"
+import { createStructuredSelector } from 'reselect'
 
+import { selectDirectorySections } from '../../redux/directory/directory.selectors'
 import MenuItem from "../menu-item/menu-item.component";
 import "./directory.styles.scss"
 
@@ -14,4 +16,8 @@ const Directory = ({ sections }) => (
   </div>
 )
 
-export default Directory
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+ })
+
+export default connect(mapStateToProps)(Directory)
